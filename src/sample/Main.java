c7f43e5a20f28430364f26e2aed05fd12ee37f3d
Guty1996 <43771601+Guty1996@users.noreply.github.com>
2018-11-26 12:01:33 -0600

    HICE ESTO

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("HOLA GUTYertyuytr");
+        primaryStage.setTitle("HOLA GUTYerty");
         primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
     }
