8ae00fb335ee4ef6630dda522b9a270293d0d129
CRS <tomas>
2018-11-27 23:22:56 -0600

    ho

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("FXMLs/sample.fxml"));
-        primaryStage.setTitle("FLORERIA DE LA MAMA QUE GRITA IIIISSSAAAAAIIIIIIIIIIII");
+        primaryStage.setTitle("FLORERIA DE LA MAMA QUE GRITA IIIISSSAAAAIIIIIIIIIIII");
         primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
     }
