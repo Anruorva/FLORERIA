5980b726e6034be918be883deac1f31953c520e6
Guty1996 <43771601+Guty1996@users.noreply.github.com>
2018-11-29 14:43:03 -0600

    HICE ESTO

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,13 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import javax.swing.*;
 
 public class Main extends Application {
 
@@ -15,10 +18,12 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 1000, 600));
         primaryStage.show();
         primaryStage.setFullScreen(true);
+
     }
 
 
     public static void main(String[] args) {
         launch(args);
+
     }
 }
