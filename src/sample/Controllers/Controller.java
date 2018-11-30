471ea3cc581fde8148cb2ced6d79a75a34265bf8
edgar <16030767@itecelaya.edu.mx>
2018-11-29 19:17:03 -0600

    muestra productos en la tabla de en medio

--- src/sample/Controllers/Controller.java
+++ src/sample/Controllers/Controller.java
@@ -1,33 +1,19 @@
 package sample.Controllers;
 
 import com.jfoenix.controls.*;
-import com.jfoenix.controls.cells.editors.TextFieldEditorBuilder;
-import com.jfoenix.controls.cells.editors.base.GenericEditableTreeTableCell;
-import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
-import com.jfoenix.transitions.hamburger.HamburgerSlideCloseTransition;
-import javafx.animation.Animation;
-import javafx.beans.binding.Bindings;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeTableColumn;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+import sample.BD.DAO.JardineriaDAO;
+import sample.BD.Model.Producto;
+import sample.BD.MySQL;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
+
 public class Controller implements Initializable {
-   @FXML
-   JFXTreeTableView treeView;
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
--- src/sample/Controllers/ControllerMedio.java
+++ src/sample/Controllers/ControllerMedio.java
@@ -1,4 +1,54 @@
 package sample.Controllers;
 
-public class ControllerMedio {
+import com.jfoenix.controls.JFXTreeTableView;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import sample.BD.DAO.JardineriaDAO;
+import sample.BD.Model.Producto;
+import sample.BD.MySQL;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerMedio implements Initializable {
+    MySQL mySQL= new MySQL();
+    JardineriaDAO jardineriaDAO= new JardineriaDAO(mySQL.getConnection());
+    @FXML
+    TableView tbl;
+    @Override
+    public void initialize(URL location, ResourceBundle resources)
+    {
+        initProductos(null);
+    }
+    public void initProductos(String id_P){
+        tbl.getItems().clear();
+        tbl.getColumns().clear();
+        TableColumn column = new TableColumn("id");
+        column.setCellValueFactory(new PropertyValueFactory<Producto, String>("id"));
+        TableColumn column2 = new TableColumn("nombre");
+        column2.setCellValueFactory(new PropertyValueFactory<Producto, String>("nombre"));
+        TableColumn column3 = new TableColumn("nombre_latin");
+        column3.setCellValueFactory(new PropertyValueFactory<Producto, String>("nombre_latin"));
+        TableColumn column4 = new TableColumn("precio_compra");
+        column4.setCellValueFactory(new PropertyValueFactory<Producto, Float>("precio_compra"));
+        TableColumn column5 = new TableColumn("precio_venta");
+        column5.setCellValueFactory(new PropertyValueFactory<Producto, Float>("precio_venta"));
+        TableColumn column6 = new TableColumn("unidades");
+        column6.setCellValueFactory(new PropertyValueFactory<Producto, Integer>("unidades"));
+        TableColumn column7 = new TableColumn("minimo_stock");
+        column7.setCellValueFactory(new PropertyValueFactory<Producto, Integer>("minimo_stock"));
+        TableColumn column8 = new TableColumn("descripcion");
+        column8.setCellValueFactory(new PropertyValueFactory<Producto, String>("descripcion"));
+        TableColumn column9 = new TableColumn("id_estado");
+        column9.setCellValueFactory(new PropertyValueFactory<Producto, String>("id_estado"));
+        tbl.getColumns().addAll(column, column2, column3, column4, column5, column6, column7, column8, column9);
+        if(id_P==null)
+            tbl.setItems(jardineriaDAO.fetchProductos());
+        else
+            tbl.setItems(jardineriaDAO.fetchProductos(id_P));
+    }
 }
+
